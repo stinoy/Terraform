bdee3849d24579eea4720088d1424a40852f5a80
stinoy <111982546+stinoy@users.noreply.github.com>
2025-02-27 19:49:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-#resource "aws_directory_service_directory" "ad" {
-#  name        = var.directory_dns_name
-#  short_name  = var.directory_netbios_name
-#  password    = var.admin_password
-#  edition     = "Enterprise"
-#  type        = "MicrosoftAD"
-#
-#  vpc_settings {
-#    vpc_id     = var.vpc_id
-#    subnet_ids = [var.subnet_1, var.subnet_2]
-#  }
-#}
-#
-#output "directory_id" {
-#  value = aws_directory_service_directory.ad.id#
 resource "aws_directory_service_directory" "ad" {
   name        = var.directory_dns_name
   short_name  = var.directory_netbios_name
@@ -25,28 +10,17 @@ resource "aws_directory_service_directory" "ad" {
     subnet_ids = [var.subnet_1, var.subnet_2]
   }
 }
-
 resource "null_resource" "enable_user_group_management" {
   depends_on = [aws_directory_service_directory.ad]
 
+  triggers = {
+    always_run = timestamp()  # Forces execution every time
+  }
+
   provisioner "local-exec" {
-    command = <<EOT
-      aws ds enable-radius --directory-id ${aws_directory_service_directory.ad.id} \
-      --radius-settings '{
-        "RadiusServers": ["192.168.1.1"],
-        "RadiusPort": 1812,
-        "RadiusRetries": 3,
-        "RadiusTimeout": 10,
-        "SharedSecret": "MyStrongSecret",
-        "AuthenticationProtocol": "PAP",
-        "DisplayLabel": "MyRadiusServer",
-        "UseSameUsername": true
-      }' --region ${var.aws_region}
-    EOT
+    command = "aws ds enable-directory-data-access --directory-id ${aws_directory_service_directory.ad.id} --region ${var.aws_region}"
   }
 }
-
 output "directory_id" {
   value = aws_directory_service_directory.ad.id
 }
-
